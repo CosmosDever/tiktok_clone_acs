012377d47f48c19558a3dacd2a9846dc91bb8944
CosmosDever <aprommhgot@gmail.com>
2024-05-10 17:34:20 +0700

    fix_responsive

--- tiktok_clone/src/app/component/Video.tsx
+++ tiktok_clone/src/app/component/Video.tsx
@@ -6,7 +6,7 @@ interface VideoProps {
 export const Video: React.FC<VideoProps> = ({ url }) => {
   return (
     <iframe
-      className=" w-[375px] h-[667px]  lg:w-[300px] lg:h-[600px] xl:w-[500px] xl:h-[800px] rounded-lg "
+      className=" w-[360px] h-[650px]  min-[414px]:w-[400px] min-[414px]:h-[850px] md:w-[700px] md:h-[1000px]  lg:w-[800px] lg:h-[1200px] xl:w-[500px] xl:h-[800px] rounded-lg "
       src={url}
     ></iframe>
   );
